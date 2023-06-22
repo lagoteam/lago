61f0e033c2995b75318bd31a35f8660da387fb16
鸿宇 <1527200768@qq.com>
2023-06-22 18:55:51 +0800

    v1.0.0

--- modules/lago/routes/web.go
+++ modules/lago/routes/web.go
@@ -8,7 +8,7 @@ import (
 
 func Web() {
 	facades.Route().Prefix("lago").Group(func(webRoute route.Route) {
-		webRoute.Get("/", func(ctx http.Context) {
+		webRoute.Get("", func(ctx http.Context) {
 			ctx.Response().Json(http.StatusOK, http.Json{
 				"name":    facades.Config().GetString("app.name"),
 				"version": facades.Config().GetString("app.version"),
